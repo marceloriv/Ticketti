df2570c53c6c1c4fa5c6c4adc71ca8d7da3ee78a
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-06 10:04:44 -0400

    style(Header): :construction: Ir agregando el carrito al Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, Badge, Stack } from 'react-bootstrap';
 import { Ticket } from 'lucide-react';
+import { Link, NavLink } from 'react-router-dom';
+// import '@styles/brand.css';
 
 const Header = () => {
   const navLinks = [
-    { name: 'Inicio', href: '#home' },
-    { name: 'Eventos', href: '#events' },
-    { name: 'Sobre Nosotros', href: '#about' },
-    { name: 'Contacto', href: '#contact' },
+    { name: 'Inicio', to: '/home' },
+    { name: 'Eventos', to: '/events' },
+    { name: 'Sobre Nosotros', to: '/about' },
+    { name: 'Contacto', to: '/contact' },
   ];
 
   return (
@@ -19,24 +20,35 @@ const Header = () => {
       className="border-bottom shadow-sm"
     >
       <Container>
-        <Navbar.Brand href="#home" className="d-flex gap-2">
-          <Ticket className="icon-ticketti me-2 " />
+        <Navbar.Brand
+          as={Link}
+          to="/home"
+          className="d-flex align-items-center gap-2"
+        >
+          <Ticket className="me-2" />
           <span className="fw-bold fs-4">Ticketti</span>
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="main-navbar" />
+
         <Navbar.Collapse id="main-navbar" className="mt-1">
           <Nav className="m-auto">
             {navLinks.map((link) => (
-              <Nav.Link key={link.name} href={link.href}>
+              <Nav.Link key={link.name} as={NavLink} to={link.to}>
                 {link.name}
               </Nav.Link>
             ))}
+            <Navbar className="btn">
+              Carrito <Badge bg="info">5</Badge>
+              <span className="visually-hidden">Conttador carrito </span>
+            </Navbar>
           </Nav>
-          <div className="">
-            <Button as={Link} to="/login" variant="primary">
+          <Stack direction="horizontal" gap={3}>
+            <Button as={Link} to={'/login'} className="primary">
               Acceso
             </Button>
-          </div>
+            {/* Contador on respecto a carrito  */}
+          </Stack>
         </Navbar.Collapse>
       </Container>
     </Navbar>
