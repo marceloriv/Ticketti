c1fc60a7c6a40b64a8247101147879585b439d74
marceloriv <ma.riveracontreras@duocuc.cl>
2026-06-10 22:24:25 -0400

    refactor: :recycle: configurar compatibilidad hacia atras mediante re-exports a src/api

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import clienteApi from '../api/clienteApi';
+
+/**
+ * Re-exportación para mantener compatibilidad con imports existentes en la aplicación
+ * que referencian a @services/api.
+ */
+export default clienteApi;
